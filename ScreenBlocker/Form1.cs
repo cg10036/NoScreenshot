87e207c9300a7d29c740997ac5cb4cb8e7e86c97
cg10036 <30382481+cg10036@users.noreply.github.com>
2019-06-07 03:12:37 +0900

    Initial commit

---
+++ ScreenBlocker/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScreenBlocker
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        static extern void SetWindowDisplayAffinity(IntPtr handle, int flag);
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetWindowDisplayAffinity(Handle, 1);
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+        }
+    }
+}
